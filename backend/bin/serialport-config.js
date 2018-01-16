d87f5455a916bc023b6f4968413efe59d45dbf4d
Moey Design <5884seiya@gmail.com>
2018-01-16 15:00:46 -0500

    fixed a wrong name variable

--- backend/bin/serialport-config.js
+++ backend/bin/serialport-config.js
@@ -4,7 +4,7 @@ if (process.env.ENV === 'MOEY_SEIYA') {
   console.log('moey seiya')
   config = {
     BAUDRATE: 115200,
-    SERIAL_PORT: '/dev/tty.usbmodem1709631'
+    SERIAL_PATH: '/dev/tty.usbmodem1709631'
   }
 } else if (process.env.ENV === 'SURFACE') {
   console.log('surface')
