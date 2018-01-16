fabec02d2af9fa804103f9ca245dd5a042905d58
Moey Design <5884seiya@gmail.com>
2018-01-16 14:26:11 -0500

    changed port from 3000 to 3100

--- others/shortcut/MoeyCrank.bat
+++ others/shortcut/MoeyCrank.bat
@@ -1,4 +1,4 @@
-start "C:\Users\moeyinc\AppData\Local\Chromium\Application\chrome.exe" http://localhost:3000
+start "C:\Users\moeyinc\AppData\Local\Chromium\Application\chrome.exe" http://localhost:3100
 cd "C:\Users\moeyinc\Documents\moey-crank\backend"
 npm run start-surface
 @pause
